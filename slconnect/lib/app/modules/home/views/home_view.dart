1bfeeec47b055bda254a98a6828a50e06403c1e6
navaneethku <navaneethku1729@gmail.com>
2023-04-22 19:14:28 +0530

    login page done

--- slconnect/lib/app/modules/home/views/home_view.dart
+++ slconnect/lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
+import 'package:lottie/lottie.dart';
+import 'package:slconnect/app/routes/app_pages.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -9,16 +10,50 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+        body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/background.gif"),
+                    fit: BoxFit.cover)),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(10, 20, 10, 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text("SL Connect",
+                      style: TextStyle(
+                        fontSize: 24,
+                      )),
+                  Lottie.asset("assets/lottie/signuplady.json"),
+                  Text("Sign Up",
+                      style: TextStyle(
+                        fontSize: 24,
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                            color: Theme.of(context).colorScheme.primary,
+                          )),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Theme.of(context).colorScheme.primary,
+                                  width: 2)),
+                          prefixIcon: Icon(Icons.call),
+                          labelText: "Phone Number",
+                          floatingLabelStyle: TextStyle(
+                              color: Theme.of(context).colorScheme.primary,
+                              backgroundColor: Theme.of(context).cardColor)),
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () => Get.toNamed(Routes.LOGIN),
+                      child: Text("Submit"))
+                ],
+              ),
+            )));
   }
 }
